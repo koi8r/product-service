328355c3ee4802cd8b37ee1c6f46e51f1f432499
Alexander Kirillov <alexander.kirillov.work@gmail.com>
2020-03-23 11:42:01 +0100

    Add h2 db and scripts to initialize data

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,14 @@
+/*
+ * Data
+ */
+
+INSERT INTO product (name, current_price, created, last_update)
+VALUES ('Apple EarPods', '16.44', NOW(), NULL);
+INSERT INTO product (name, current_price, created, last_update)
+VALUES ('Apple Macbook Air (2019) ', '1005.88', NOW(), NULL);
+INSERT INTO product (name, current_price, created, last_update)
+VALUES ('BenQ TK800M 4K DLP Beamer', '1110', NOW(), NULL);
+INSERT INTO product (name, current_price, created, last_update)
+VALUES ('LG PH550G HD Mini Beamer', '369', NOW(), NULL);
+INSERT INTO product (name, current_price, created, last_update)
+VALUES ('Canon EOS 2000D', '375.99', NOW(), NULL);
