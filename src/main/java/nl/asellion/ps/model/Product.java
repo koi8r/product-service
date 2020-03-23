cb71ccf727eb5a9ee7591d2ebd963c0f8673a244
Alexander Kirillov <alexander.kirillov.work@gmail.com>
2020-03-23 11:04:24 +0100

    Add Product entity

--- src/main/java/nl/asellion/ps/model/Product.java
+++ src/main/java/nl/asellion/ps/model/Product.java
@@ -1,11 +1,51 @@
 package nl.asellion.ps.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Alexander Kirillov
  */
 
-@Builder
-public class Product {
+@Entity
+@Table(name = "product")
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode
+public final class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @EqualsAndHashCode.Exclude
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", length = 100, unique = true, nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "current_price", precision = 10, scale = 2, nullable = false)
+    private BigDecimal current_price;
+
+    @NotNull
+    @Column(name = "last_update", nullable = false)
+    private LocalDateTime lastUpdate;
+
 }
