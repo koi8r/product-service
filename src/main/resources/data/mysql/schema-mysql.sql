8e5c7ac7d783f532cda8130330b9301eba8bdcea
Alexander Kirillov <alexander.kirillov.work@gmail.com>
2020-03-25 11:28:34 +0100

    Add support, schemas and data for mysql

---
+++ src/main/resources/data/mysql/schema-mysql.sql
@@ -0,0 +1,20 @@
+/*
+ * Structure
+ */
+
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product (
+    `id` bigint(20) unsigned NOT NULL auto_increment,
+    `name` varchar(100) NOT NULL,
+    `current_price` decimal(10,2) NOT NULL,
+    `last_update` datetime NOT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT `uq_product_name` UNIQUE (`name`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# INSERT INTO product (name, current_price, created, last_update) VALUES ('Apple EarPods', '16.44', NOW(), NULL);
+# INSERT INTO product (name, current_price, created, last_update) VALUES ('Apple Macbook Air (2019) MVFK2', '1005.88', NOW(), NULL);
+# INSERT INTO product (name, current_price, created, last_update) VALUES ('BenQ TK800M 4K DLP Beamer', '1110', NOW(), NULL);
+# INSERT INTO product (name, current_price, created, last_update) VALUES ('LG PH550G HD Mini Beamer', '369', NOW(), NULL);
+# INSERT INTO product (name, current_price, created, last_update) VALUES ('Canon EOS 2000D', '375.99', NOW(), NULL);
